481f860a228cc47e833acea57373fd42621a18b3
Davi Aquino <djejaquino@gmail.com>
2021-09-08 10:04:50 -0300

    chore: add SKIP_INSTALL flag

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,13 @@ current_directory="$PWD"
 
 cd $(dirname $0)
 
-yarn install --frozen-lockfile
+if [ "$SKIP_INSTALL" = true ] || [ "$SKIP_INSTALL" = 1 ]
+then
+  echo SKIP_INSTALL is set, skipping dependency installation...
+else
+  yarn install --frozen-lockfile
+fi
+
 yarn build
 
 cd "$current_directory"
