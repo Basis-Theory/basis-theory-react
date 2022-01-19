bca87960bb4e0af90378d0d3ac2d453fee29a308
Davi Aquino <djejaquino@gmail.com>
2022-01-19 13:22:50 -0300

    chore: wip card element

--- src/elements/CardElement.tsx
+++ src/elements/CardElement.tsx
@@ -1,78 +1,96 @@
 import React, { FC, useEffect, useState } from 'react';
 import type {
-  CardElement as Element,
-  UpdateCardElementOptions,
+  BaseElement,
+  BasisTheoryElements,
+  CardElement as ICardElement,
   ElementEventListener,
+  ElementStyle,
+  EventType,
+  Subscription,
 } from '@basis-theory/basis-theory-elements-interfaces/elements';
-import { useSuperElements } from './Elements';
+import { useBasisTheory } from '../core';
+import type { BasisTheoryReact, ElementMapper } from '../core/types';
 
-interface Props {
+interface CardElementProps {
   id: string;
+  bt?: BasisTheoryReact;
+  style?: ElementStyle;
+  disabled?: boolean;
   onChange?: ElementEventListener<'change'>;
   onFocus?: ElementEventListener<'focus'>;
   onBlur?: ElementEventListener<'blur'>;
   onReady?: ElementEventListener<'ready'>;
   onKeyDown?: ElementEventListener<'keydown'>;
-  options?: UpdateCardElementOptions;
 }
 
-export const CardElement: FC<Props> = ({
+const useListener = <T extends EventType, E extends BaseElement<unknown>>(
+  eventType: T,
+  element?: E,
+  listener?: ElementEventListener<T>
+): void =>
+  useEffect(() => {
+    let subscription: Subscription;
+
+    if (element && listener) {
+      subscription = element.on(eventType, listener);
+    }
+
+    return () => subscription?.unsubscribe();
+  }, [element, eventType, listener]);
+
+export const CardElement: FC<CardElementProps> = ({
   id,
+  bt: btFromProps,
+  style,
+  disabled,
+  onReady,
   onChange,
   onFocus,
   onBlur,
-  onReady,
   onKeyDown,
-  options,
 }) => {
-  const { elements, indexElement, disposeElement } = useSuperElements();
-  const [element, setElement] = useState<Element>();
+  const { bt: btFromContext } = useBasisTheory();
+  const [element, setElement] = useState<ICardElement>();
+
+  const bt = (btFromProps || btFromContext) as
+    | (BasisTheoryElements & ElementMapper)
+    | undefined;
 
   useEffect(() => {
-    if (elements && !element) {
-      const card = elements.create('card', options);
-
-      if (onChange) {
-        card.on('change', onChange);
-      }
-
-      if (onBlur) {
-        card.on('blur', onBlur);
-      }
-
-      if (onFocus) {
-        card.on('focus', onFocus);
-      }
-
-      if (onReady) {
-        card.on('ready', onReady);
-      }
-
-      if (onKeyDown) {
-        card.on('keydown', onKeyDown);
-      }
+    if (bt && !element) {
+      const card = bt.createElement('card', {
+        style,
+        disabled,
+      });
 
       card.mount(`#${id}`);
       setElement(card);
-      indexElement(id, card);
+      bt.indexElement(id, card);
     }
 
     return (): void => {
       if (element?.mounted) {
         element.unmount();
-        disposeElement(id);
+        bt?.disposeElement(id);
         setElement(undefined);
       }
     };
+  }, [bt, id, element, style, disabled]);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [elements, id, element]);
+  useListener('ready', element, onReady);
+  useListener('change', element, onChange);
+  useListener('focus', element, onFocus);
+  useListener('blur', element, onBlur);
+  useListener('keydown', element, onKeyDown);
 
   useEffect(() => {
-    if (options && element?.mounted) {
-      element.update(options);
+    if (element?.mounted) {
+      element.update({
+        style,
+        disabled,
+      });
     }
-  }, [options, element]);
+  }, [element, style, disabled]);
 
   return <div id={id} />;
 };
