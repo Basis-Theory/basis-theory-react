95965223f192dca7b88887490eebe738aeeae9e9
Davi Aquino <djejaquino@users.noreply.github.com>
2022-04-20 10:34:37 -0300

    fix: export common errors (#70)

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,5 @@
 export { useBasisTheory } from './useBasisTheory';
 export { BasisTheoryProvider } from './BasisTheoryProvider';
 export type { BasisTheoryReact } from './types';
+export { BasisTheoryApiError } from '@basis-theory/basis-theory-elements-interfaces/sdk';
+export { BasisTheoryValidationError } from '@basis-theory/basis-theory-elements-interfaces/elements';
