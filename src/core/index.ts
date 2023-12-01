850e14e760fe9754e972fb1dc6e5da2473811b51
Kevin Peraza <kevin@basistheory.com>
2023-12-01 14:42:32 -0600

    feat: adds support for custom cardTypes (#223)
    * feat: adds cardTypes for custom bin validation
    
    * feat: update btjs

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,3 +5,18 @@ export {
   BasisTheoryApiError,
   BasisTheoryValidationError,
 } from '@basis-theory/basis-theory-js/common';
+export {
+  AMERICAN_EXPRESS,
+  DEFAULT_CARD_TYPES,
+  DINERS_CLUB,
+  DISCOVER,
+  ELO,
+  HIPER,
+  HIPERCARD,
+  JCB,
+  MAESTRO,
+  MASTERCARD,
+  MIR,
+  UNION_PAY,
+  VISA,
+} from '@basis-theory/basis-theory-js/types/elements';
